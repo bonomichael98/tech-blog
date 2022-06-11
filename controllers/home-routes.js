905c762ca3fced6017e6ae439f37e133d7f3ccb9
michael <shinlin09@gmail.com>
2022-06-11 17:19:48 -0400

    middleware issue

---
+++ controllers/home-routes.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  Post.findAll({
+    attributes: [
+        'id',
+        'title',
+        'post_body',
+        'user_id',
+        'created_at'
+    ],
+    include : [
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+  })
+  .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json
+  })
+});
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+        id: req.params.id
+    },
+    attributes: [
+        'id',
+        'title',
+        'post_body',
+        'user_id',
+        'created_at'
+    ],
+    include : [
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+})
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = dbPostData.get({ plain: true });
+
+    res.render('single-post', {
+      post,
+      loggedIn: req.session.loggedIn
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
+
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/signup', (req,res) => {
+  res.render('signup')
+})
+
+router.get('/new-post', (req,res) => {
+  res.render('add-post')
+});
+
+module.exports = router;
