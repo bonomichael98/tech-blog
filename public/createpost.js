38219468a296221609c1d0b8abf1edeec5f9f461
michael <shinlin09@gmail.com>
2022-06-28 13:52:55 -0400

    heroku deployment

--- public/createpost.js
+++ public/createpost.js
@@ -1,21 +1,16 @@
 async function createPostHandler(event) {
     event.preventDefault();
-
     const title = document.querySelector("input[name=post-title]").value.trim();
     const post_body = document.querySelector("textarea[name=post-content").value.trim();
-
         const response = await fetch("/api/post/", {
             method: 'POST',
             body: JSON.stringify({
-                title,
-                post_body,
+                title, post_body
             }),
             headers: {
                 'Content-Type': 'application/json'
             }
-
         });
-
         if (response.ok) {
                 document.location.replace('/');
         } else {
@@ -23,5 +18,4 @@ async function createPostHandler(event) {
         };
     console.log('click')
 };
-
 document.querySelector('.login-signup-btn').addEventListener('click', createPostHandler);
